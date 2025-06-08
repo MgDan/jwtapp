88f544263a2b033aab99062cb452ff44e3911bcf
MgDan <danielvzzesc@gmail.com>
2025-06-08 11:27:39 -0600

    added missing model

--- src/main/java/org/dandroid/jwtapp/persistence/ProductRepository.java
+++ src/main/java/org/dandroid/jwtapp/persistence/ProductRepository.java
@@ -4,6 +4,7 @@ import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
 import jakarta.transaction.Transactional;
+import org.dandroid.jwtapp.persistence.models.Product;
 
 @ApplicationScoped
 public class ProductRepository {
---
+++ src/main/java/org/dandroid/jwtapp/persistence/models/Product.java
@@ -0,0 +1,12 @@
+package org.dandroid.jwtapp.persistence.models;
+
+import java.util.Date;
+
+public class Product {
+    private Long id;
+    private String nombre;
+    private double precio;
+    private Date fechaResgistro;
+    private Long categoriaId;
+    private String sku;
+}
